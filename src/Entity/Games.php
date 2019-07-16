7aa908a758e481a32f6c6d78136fd4f666dc8d15
paulo-guerin <43541688+paulo-guerin@users.noreply.github.com>
2019-07-16 10:12:32 +0200

    Games table added without foreign key

---
+++ src/Entity/Games.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GamesRepository")
+ */
+class Games
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $minplayer;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $maxplayer;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $picture;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getMinplayer(): ?int
+    {
+        return $this->minplayer;
+    }
+
+    public function setMinplayer(?int $minplayer): self
+    {
+        $this->minplayer = $minplayer;
+
+        return $this;
+    }
+
+    public function getMaxplayer(): ?int
+    {
+        return $this->maxplayer;
+    }
+
+    public function setMaxplayer(?int $maxplayer): self
+    {
+        $this->maxplayer = $maxplayer;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getTime(): ?string
+    {
+        return $this->time;
+    }
+
+    public function setTime(?string $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
