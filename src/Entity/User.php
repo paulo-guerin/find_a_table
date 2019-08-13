774f577f1194826989bab4300c836ba7dc5bca90
paulo-guerin <43541688+paulo-guerin@users.noreply.github.com>
2019-08-13 15:05:15 +0200

    columns birthdate, firstname, lastname, gender, profilepicture added to user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -24,4 +24,110 @@ class User extends BaseUser
         parent::__construct();
         // your own logic
     }
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $firstname;
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $lastname;
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param mixed $lastname
+     */
+    public function setLastname($lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $gender;
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender): void
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $birthdate;
+
+    /**
+     * @return mixed
+     */
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+
+    /**
+     * @param mixed $birthdate
+     */
+    public function setBirthdate($birthdate): void
+    {
+        $this->birthdate = $birthdate;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $profilepicture;
+
+    /**
+     * @return mixed
+     */
+    public function getProfilepicture()
+    {
+        return $this->profilepicture;
+    }
+
+    /**
+     * @param mixed $profilepicture
+     */
+    public function setProfilepicture($profilepicture): void
+    {
+        $this->profilepicture = $profilepicture;
+    }
+
 }
